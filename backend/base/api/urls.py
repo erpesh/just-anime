67ab47639cc85beda4b66ee3d91d2902669f8731
erpesh <89254828+erpesh@users.noreply.github.com>
2022-04-05 21:53:00 +0300

    9 commit

--- backend/base/api/urls.py
+++ backend/base/api/urls.py
@@ -6,6 +6,7 @@ from .views import MyTokenObtainPairView
 
 urlpatterns = [
     path('', views.getRoutes),
+    path('notes', views.getNotes),
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- backend/base/api/views.py
+++ backend/base/api/views.py
@@ -1,7 +1,11 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+from .serializers import NoteSerializer
+from ..models import Note
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -26,3 +30,13 @@ def getRoutes(request):
     ]
 
     return Response(routes)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getNotes(request):
+    # user = request.user
+    # notes = user.note_set.all()
+    # Also can be as above
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
