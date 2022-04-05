f26a94e563bb8052ed7298ef87be676d9594205a
erpesh <89254828+erpesh@users.noreply.github.com>
2022-04-05 18:45:34 +0300

    7 commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import {BrowserRouter as Router} from 'react-router-dom'
-import MyRouter from "./router/MyRouter";
+import MyRouter from "./utils/MyRouter";
+import Header from "./components/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-          <MyRouter/>
-      </Router>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Header/>
+                <MyRouter/>
+            </div>
+        </Router>
+
+    );
 }
 
 export default App;
